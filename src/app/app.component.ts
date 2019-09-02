ece8cec59f622894a10bfe9d0d1cf74711ac2c20
Cyrille Djoko Djilo <cyrilledjoko@gmail.com>
2019-09-02 17:08:33 -0400

    setup to match training + first component with @component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fundamentals';
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
