ff6001789f0995bacc23890172d68b7048ebb36e
Cyrille Djoko <cyrilledjoko@gmail.com>
2019-09-14 01:26:44 -0400

    ngOnit lifecycle hook

--- src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ src/app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Passenger } from '../../models/passenger';
 
 @Component({
@@ -8,42 +8,44 @@ import { Passenger } from '../../models/passenger';
 })
 export class PassengerDashboardComponent implements OnInit {
 
-  passengers: Passenger[] = [{
-    id: 1,
-    fullname: 'Stephen',
-    checkedIn: true,
-    checkInDate: 1490742000000,
-    children: null
-  }, {
-    id: 2,
-    fullname: 'Rose',
-    checkedIn: false,
-    checkInDate: null,
-    children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-  }, {
-    id: 3,
-    fullname: 'James',
-    checkedIn: true,
-    checkInDate: 1491606000000,
-    children: null
-  }, {
-    id: 4,
-    fullname: 'Louise',
-    checkedIn: true,
-    checkInDate: 1488412800000,
-    children: [{ name: 'Jessica', age: 1 }]
-  }, {
-    id: 5,
-    fullname: 'Tina',
-    checkedIn: false,
-    checkInDate: null,
-    children: null
-  }];
+  passengers: Passenger[];
 
 
   constructor() { }
 
   ngOnInit() {
+    console.log('ngOnInit')
+    this.passengers = [{
+      id: 1,
+      fullname: 'Stephen',
+      checkedIn: true,
+      checkInDate: 1490742000000,
+      children: null
+    }, {
+      id: 2,
+      fullname: 'Rose',
+      checkedIn: false,
+      checkInDate: null,
+      children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
+    }, {
+      id: 3,
+      fullname: 'James',
+      checkedIn: true,
+      checkInDate: 1491606000000,
+      children: null
+    }, {
+      id: 4,
+      fullname: 'Louise',
+      checkedIn: true,
+      checkInDate: 1488412800000,
+      children: [{ name: 'Jessica', age: 1 }]
+    }, {
+      id: 5,
+      fullname: 'Tina',
+      checkedIn: false,
+      checkInDate: null,
+      children: null
+    }];
   }
 
 }
