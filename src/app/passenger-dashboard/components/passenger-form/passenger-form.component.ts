b18a949ede60a5a51204483b126801b85a2e76d6
Cyrille Djoko <cyrilledjoko@gmail.com>
2019-11-16 17:38:56 -0500

    select option rendering and ngValue

--- src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ src/app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Passenger } from '../../models/passenger';
+import { Baggage } from '../../models/baggage';
 
 @Component({
   selector: 'passenger-form',
@@ -9,6 +10,12 @@ import { Passenger } from '../../models/passenger';
 export class PassengerFormComponent implements OnInit {
   @Input()
   detail: Passenger;
+  baggage: Baggage[] = [
+    { key: 'none', value: 'No baggage' },
+    { key: 'hand-only', value: 'Hand baggage' },
+    { key: 'hold-only', value: 'Hold baggage' },
+    { key: 'hand-hold', value: 'Hand and Hold baggage' }
+  ];
   constructor() { }
 
   ngOnInit() {
